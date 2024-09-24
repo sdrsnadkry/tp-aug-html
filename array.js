f4ece4a0424f8e440cb93c8a4c48635d13b01fc1
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-09-24 11:33:22 +0545

    loops

---
+++ array.js
@@ -0,0 +1,25 @@
+const n1 = 2;
+
+const n2 = 3;
+const n3 = 4;
+
+const s1 = {
+  name: "Jane",
+};
+
+console.log(s1.name)
+
+//array
+//comma separated values
+
+// const numbers = [n1, n2, n3];
+const numbers = ["1", true, 3, 4, s1];
+
+
+//access using index inside [] -> [0]
+
+console.log(numbers[4].name)
+
+const names = ["Jane", "Jone", "Jasmine"];
+
+console.log(names[1])
---
+++ object.js
@@ -0,0 +1,33 @@
+const s1Name = 'Jane';
+// const s1Age = 22;
+// const s1City = 'Dhaka';
+
+
+// const s2Name = 'Jone';
+// const s2Age = 23;
+// const s2City = 'Dhaka';
+
+// const s3Name = 'Jone';
+// const s3Age = 23;
+// const s3City = 'Dhaka';
+
+//object
+//key value pair
+
+const s1 = {
+  name: "Jane",
+  age: 22,
+  isMale: false,
+  "primary-address": 'Dhaka',
+  secondaryAddress: 'Chittagong',
+};
+
+console.log(s1.name, s1.age, s1["primary-address"])
+
+const s2 = {
+  name: "Jone",
+  age: 23,
+  city: "Dhaka",
+  isMale: true,
+};
+
